916c967a5b7b53221b211c656547ef81f2c0b056
Ryo NAKAMURA <r7kamura@gmail.com>
2010-07-20 07:09:54 +0900

    first commit & test complete

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,63 @@
-# implement this
+package Sorter;
+use strict;
+use warnings;
+
+sub new {
+  my $class = shift;
+  my $self = {
+    values => []
+  };
+  bless $self, $class;
+}
+
+sub set_values {
+  my $self = shift;
+  my @values = @_;
+  $self->{values} = \@values;
+}
+
+sub get_values {
+  my $self = shift;
+  my $array = $self->{values};
+  return @{$array};
+}
+
+sub sort {
+  my $self = shift;
+  my $array = $self->{values};
+  $self->quick_sort(0, $#{$array}) if ($#{$array}) > 0;
+}
+
+sub quick_sort {
+  my $self = shift;
+  my ($left, $right) = @_;
+  
+  if ($left < $right) {
+    my ($i, $j) = ($left, $right);
+    my $array = $self->{values};
+    my $center = int(($left + $right) / 2);
+    my $pivot = $array->[$center];
+    
+    while (1) {
+      $i++ while($array->[$i] < $pivot);
+      $j-- while($array->[$j] > $pivot);
+      last if ($i >= $j);
+      $self->swap($i, $j);
+      $i++; $j--;
+    }
+    
+    $self->quick_sort($left, $i - 1)  if ($left < $i-1);
+    $self->quick_sort($j + 1, $right) if ($j+1 < $right);
+  }
+}
+
+sub swap {
+  my $self = shift;
+  my ($i, $j) = @_;
+  my $array = $self->{values};
+  my $tmp = $array->[$i];
+  $array->[$i] = $array->[$j];
+  $array->[$j] = $tmp;
+}
+
+1;
---
+++ lib/spike.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Sorter;
+
+my $sorter = Sorter->new;
+$sorter->sort;
+$sorter->get_values;
