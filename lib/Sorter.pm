5ae296925d20e0fc42eb06809c6d20cd89128fb8
Ryo NAKAMURA <r7kamura@gmail.com>
2010-07-21 21:42:12 +0900

    left/rightの振り分け部分少し簡潔にした

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -22,29 +22,22 @@ sub get_values {
 
 sub sort {
   my $self = shift;
-  
-  return if $self->get_values < 1;
+  return if $self->get_values <= 1;
   
   my $pivot = $self->_pivot;
-  my $left = __PACKAGE__->new;
-  my $right = __PACKAGE__->new;
-  $left->set_values($self->_partition($pivot, -1));
-  $right->set_values($self->_partition($pivot, 1));
-  $left->sort;
-  $right->sort;
-  $self->set_values($left->get_values, $pivot, $right->get_values);
+  my (@lvals, @rvals);
+  for my $value ($self->get_values) {
+    ($value < $pivot) ? push(@lvals, $value) : push(@rvals, $value);
+  }
+  my $l = __PACKAGE__->new;
+  my $r = __PACKAGE__->new;
+  $l->set_values(@lvals);
+  $r->set_values(@rvals);
+  $l->sort;
+  $r->sort;
+  $self->set_values($l->get_values, $pivot, $r->get_values);
 }
 
-sub _partition {
-  my ($self, $pivot, $comp) = @_;
-  
-  if ($comp < 0) {
-    grep { $_ < $pivot } $self->get_values;
-  }
-  else {
-    grep { $_ >= $pivot } $self->get_values;
-  }
-}
 
 sub _pivot {
   my $self = shift;
