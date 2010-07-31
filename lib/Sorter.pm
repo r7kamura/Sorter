cdeeaaf615c942f3e5da59a486ad04b9f60005b7
Ryo NAKAMURA <r7kamura@gmail.com>
2010-08-01 04:22:49 +0900

    _pivotメソッドなど無駄な部分削除

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -3,47 +3,36 @@ use strict;
 use warnings;
 
 sub new {
-  my $class = shift;
-  my $self = {
-    values => []
-  };
-  bless $self, $class;
+  bless { values => [] }, shift;
 }
 
 sub set_values {
   my $self = shift;
   $self->{values} = [@_];
+  $self;
 }
 
 sub get_values {
-  my $self = shift;
-  @{$self->{values}};
+  @{shift->{values}};
 }
 
 sub sort {
   my $self = shift;
-  return if $self->get_values <= 1;
+  my @values = $self->get_values;
+  return $self if @values <= 1;
   
-  my $pivot = $self->_pivot;
+  my $pivot = splice(@values, int(@values / 2), 1);
   my (@lvals, @rvals);
-  for my $value ($self->get_values) {
+  for my $value (@values) {
     ($value < $pivot) ? push(@lvals, $value) : push(@rvals, $value);
   }
   my $l = __PACKAGE__->new;
   my $r = __PACKAGE__->new;
-  $l->set_values(@lvals);
-  $r->set_values(@rvals);
-  $l->sort;
-  $r->sort;
-  $self->set_values($l->get_values, $pivot, $r->get_values);
-}
-
-sub _pivot {
-  my $self = shift;
-  my @values = $self->get_values;
-  my $pivot = splice(@values, int(@values/2), 1);
-  $self->set_values(@values);
-  $pivot;
+  $self->set_values(
+    $l->set_values(@lvals)->sort->get_values,
+    $pivot,
+    $r->set_values(@rvals)->sort->get_values
+  );
 }
 
 1;
